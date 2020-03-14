f37ecc05f9c76b02e6b4b2e006c0e70aef42ddce
ald2424 <abigaild2424@gmail.com>
2020-03-14 16:20:34 -0400

    Nav Bar up and running

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
+import Portfolio from "./pages/portfolio";
+import NavTabs from "./components/NavTabs";
 import './App.css';
 
 function App() {
   return (
-    <About/>
+    <Router>
+    <div>
+      <NavTabs>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/" component={Portfolio} />
+      </NavTabs>
+      <Route exact path="/" component={About} />
+      <Route exact path="/portfolio" component={Portfolio}/>
+    </div>
+  </Router>
   );
 }
 
---
+++ react-portfolio/src/components/NavTabs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs(){
+    
+    const location = useLocation();
+
+    return(
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/portfolio"
+          className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+        >
+          Portfolio
+        </Link>
+      </li>
+    </ul>
+    )
+}
+export default NavTabs;
